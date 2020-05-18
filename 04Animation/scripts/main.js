be501054b44b82d8821529d2e19424e1199e68cc
Denise05 <61313094+Denise05@users.noreply.github.com>
2020-05-18 10:30:17 +0200

    b2

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,8 +5,8 @@
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed;
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
+
 	console.log(myOtherBox.object3D.rotation.x);
  }
 
